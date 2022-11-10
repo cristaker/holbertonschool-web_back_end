de926a09e180d97dc4111162c0dc9f9db9e57136
cristian <cristaker1927@hotmail.com>
2022-11-10 18:20:32 -0500

    project

--- 0x0E-ES6_basic/100-createIteratorObject.js
+++ 0x0E-ES6_basic/100-createIteratorObject.js
@@ -1,23 +1,3 @@
 export default function createIteratorObject(report) {
-    let iterable = [];
-  
-    if (!report.allEmployees || typeof report.allEmployees !== 'object') {
-      return iterable;
-    }
-  
-    //   for (const r of Object.values(report.allEmployees)) {
-    //     iterable.push(...r);
-    //   }
-  
-    iterable = {
-      * [Symbol.iterator]() {
-        for (const value of Object.values(report.allEmployees)) {
-          for (const i of value) {
-            yield i;
-          }
-        }
-      },
-    };
-  
-    return iterable;
-  }
+  return
+}
